8377df80fe72e11cefc61368bc9ea9ab5e55ce26
Airlon Servulo <airlon.servulo@outlook.com>
2020-12-02 16:58:42 -0300

    Projecto criado

---
+++ src/main/java/com/aleloprojeto/projetoalelo/ProjetoAleloApplication.java
@@ -0,0 +1,15 @@
+package com.aleloprojeto.projetoalelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAleloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAleloApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/aleloprojeto/projetoalelo/ProjetoAleloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleloprojeto.projetoalelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAleloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
