14435ca52e4839122d116b8a638020ca9ee9ae7e
Airlon Servulo <airlon.servulo@outlook.com>
2020-12-04 15:28:28 -0300

    Atualizando codigo para requisição PUT

--- src/main/java/com/aleloprojeto/projetoalelo/domain/Produto.java
+++ src/main/java/com/aleloprojeto/projetoalelo/domain/Produto.java
@@ -12,7 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable { 
@@ -24,7 +24,8 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
-	@JsonBackReference
+	@JsonIgnore
+	//@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", 
 	    joinColumns =@JoinColumn(name = "produto_id"), 
@@ -72,7 +73,7 @@ public class Produto implements Serializable {
 	}
 
 	public void setCategorias(List<Categoria> categorias) {
-		categorias = categorias;
+		this.categorias = categorias;
 	}
 
 	@Override
--- src/main/java/com/aleloprojeto/projetoalelo/resources/CategoriaResource.java
+++ src/main/java/com/aleloprojeto/projetoalelo/resources/CategoriaResource.java
@@ -4,7 +4,9 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,14 +24,14 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET) //Requisição para obter dados
+	@GetMapping(value="/{id}") //Requisição para obter dados
 	public ResponseEntity<?> find( @PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 				
 	}
 	
-	@RequestMapping(method=RequestMethod.POST)
+	@PostMapping(consumes = "application/json")
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
